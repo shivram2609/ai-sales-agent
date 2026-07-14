2b13da937afa5b19c0f7153e12b0e463da9796c6
shivam sharma <shivam@zestminds.com>
2026-07-14 12:15:55 +0530

    add email schedule command

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('app:about', function () {
     $this->info('AI Sales Agent Laravel app is ready.');
 });
+Schedule::command('sending:process-queue')->everyMinute()->withoutOverlapping();
